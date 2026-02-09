ec53c1671e7342b2aac1d5a5d3110ad577dc2704
Loren <loren@example.com>
2026-02-09 14:38:01 -0800

    "Fix_deployment_config"

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
+  base: '/constraints_card_deck/',
   plugins: [
     react(),
     VitePWA({
